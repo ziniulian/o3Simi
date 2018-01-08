085fa364da82cea892f013bc9d2cb9528c0adbb7
ziniulian <ziniulian@163.com>
2018-01-08 10:02:15 +0800

    占坑小Demo

---
+++ index.js
@@ -0,0 +1,46 @@
+// LZR 模块加载
+require("lzr");
+
+// LZR 子模块加载
+LZR.load([
+	"LZR.Node.Srv",
+	"LZR.HTML"
+]);
+
+// 域名
+var dmsrv = {
+	main: LZR.HTML.domain
+};
+
+// 服务的实例化
+var srv = new LZR.Node.Srv ({
+	ip: process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
+	port: process.env.OPENSHIFT_NODEJS_PORT || 80
+});
+
+// LZR库文件访问服务
+srv.ro.setStaticDir("/myLib/", LZR.curPath);
+
+// LOGO图片
+srv.ro.get("/favicon.ico", function (req, res) {
+	res.redirect(dmsrv.main + "/favicon.ico");
+});
+
+// srv.use("/Simi/", require("./Simi"));
+srv.ro.get("/", function (req, res) {
+	res.send("Hello <a href='http://www.ziniulian.tk/'>LZR</a>");
+});
+
+// 追踪器
+srv.ro.get("/trace.js", function (req, res) {
+	res.redirect(dmsrv.main + "/js/trace.js");
+});
+
+// 收尾处理
+srv.use("*", function (req, res) {
+	res.redirect(dmsrv.main + "/Err");
+});
+
+// 服务启动
+srv.start();
+console.log("LZRgu start " + srv.ip + ":" + srv.port);
